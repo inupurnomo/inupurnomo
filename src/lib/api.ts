15ddedb9101679d86aa41e4f6e51726095d6b09e
Ilham Ibnu Purnomo <social.inupurnomo@gmail.com>
2023-05-27 00:41:22 +0700

    delete netlify cms

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -12,8 +12,8 @@ type PostItems = {
 };
 
 export function getPostBySlug(slug: string, fields: string[] = []): PostItems {
-  const realSlug = slug.replace(/\.md$/, '');
-  const fullPath = join(POSTS_PATH, `${realSlug}.md`);
+  const realSlug = slug.replace(/\.mdx$/, '');
+  const fullPath = join(POSTS_PATH, `${realSlug}.mdx`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
   const { data, content } = matter(fileContents);
 
